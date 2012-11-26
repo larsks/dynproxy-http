f00c53ebdac10d740db3b9a269a636351b2d4268
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-11-26 12:35:09 -0500

    - support "enabled" attribute - generic result-to-dict

--- dynproxy/model.py
+++ dynproxy/model.py
@@ -10,20 +10,23 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.interfaces import PoolListener
 from sqlalchemy.sql.expression import func, select
+import yaml
 
 Base = declarative_base()
 Session = sessionmaker()
 
-class Backend (Base):
+class DictSerializable(object):
+    def as_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            result[key] = str(getattr(self, key))
+        return result
+
+class Backend (Base, DictSerializable):
     __tablename__ = 'backends'
     name = Column(String, primary_key=True)
     ipaddr = Column(String, nullable=True)
-
-    def as_dict(self):
-        return {
-                'name': str(self.name),
-                'ipaddr': str(self.ipaddr),
-                }
+    enabled = Column(Boolean, default=False)
 
 def initmodel(dburi, echo=False):
     engine = create_engine(dburi, echo=echo)
